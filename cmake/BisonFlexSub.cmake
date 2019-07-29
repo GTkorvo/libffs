31fe62e55958bec80bdce73fc8a6d5be7d7262d5
Julien Schueller <schueller@phimeca.com>
2019-07-29 20:12:52 +0200

    CMake: Fix parser rule

--- cmake/BisonFlexSub.cmake
+++ cmake/BisonFlexSub.cmake
@@ -1,10 +1,9 @@
 FUNCTION (SETUP_BISON_FLEX_SUB)
 
-IF ((${CMAKE_SYSTEM_NAME} STREQUAL "Darwin") OR
-   (${CMAKE_SYSTEM_NAME} STREQUAL "Linux"))
-   set (BISON_FLEX_PRECOMPILE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/cod/pregen_source/Linux")
-elseif (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+IF (WIN32)
    set (BISON_FLEX_PRECOMPILE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/cod/pregen_source/Windows")
+elseif (UNIX)
+   set (BISON_FLEX_PRECOMPILE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/cod/pregen_source/Linux")
 else()
    if (NOT BISON_FOUND)
       message (FATAL_ERROR "Bison was not found and no pregenerated Bison/Flex"
